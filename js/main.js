bca89bdea3ecbf90e48847942719eefc56d1ec3d
Samuel Carneiro <samuelcarneiro.al@gmail.com>
2024-07-23 20:07:18 -0300

    Alteração da foto do perfil

--- js/main.js
+++ js/main.js
@@ -3,13 +3,13 @@ function toggleMode() {
     const html = document.documentElement
 
     // Se no document html contém a classe light
-    //if(html.classList.contains('light')) {
-        //Pegar o html (variável a cima) e remover a classe light do document
+    // if(html.classList.contains('light')) {
+    //     Pegar o html (variável a cima) e remover a classe light do document
     //    html.classList.remove('light')
-    //} else {
-        // Se não conter a a classe light, adiciona-a no document
+    // } else {
+    //     Se não conter a a classe light, adiciona-a no document
     //    html.classList.add('light')
-    //}
+    // }
 
     // A função toggle já faz o que foi declarado na função a cima...
     html.classList.toggle('light')
